f68b0ca2d4e8b50873022bf32f3a7da331dc587e
Yugandhartripathi <tripathi.yugandhar@gmail.com>
2019-10-13 09:54:53 +0530

    Working without key and sms
    Signed-off-by: Yugandhartripathi <tripathi.yugandhar@gmail.com>

---
+++ app/src/main/java/com/hovar/googlelocationbackground/MainActivity.java
@@ -0,0 +1,220 @@
+ package com.hovar.googlelocationbackground;
+
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseError;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+ public class MainActivity extends AppCompatActivity {
+
+     static MainActivity instance;
+     LocationRequest locationRequest;
+     FusedLocationProviderClient fusedLocationProviderClient;
+     TextView txt_location;
+     private Button mButton;
+     private EditText Name;
+     private EditText Telefonnummer;
+     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS =0 ;
+
+     public static MainActivity getInstance(){
+         return instance;
+     }
+     private FirebaseAuth mAuth;
+     private FirebaseAnalytics mFirebaseAnalytics;
+     FirebaseDatabase mDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        mDatabase = FirebaseDatabase.getInstance();
+        mDatabase.setPersistenceEnabled(true);
+        IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+
+        Name = (EditText)findViewById(R.id.namae);
+        Telefonnummer = (EditText)findViewById(R.id.telefonnummer);
+        mButton = findViewById(R.id.addButton);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                String nomine = Name.getText().toString();
+                String numerus = Telefonnummer.getText().toString();
+                Log.i("Button", nomine);
+                Log.i("Button", "Button Clicked");
+                Log.i("Button", numerus);
+                // Write a message to the database
+                FirebaseUser currUser = mAuth.getCurrentUser();
+                if(currUser!=null) {
+                    String currUID = currUser.getUid();
+                    mDatabase.getReference("users").child(currUID).child("Contacts").setValue(true);
+                    mDatabase.getReference("contacts").child(currUID).child(nomine).setValue(numerus);
+                }
+            }
+        });
+
+        txt_location = (TextView)findViewById(R.id.txt_location);
+        instance = this;
+
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        Toast.makeText(MainActivity.this,"Location access",Toast.LENGTH_SHORT).show();
+                        updateLocation();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(MainActivity.this,"You must accept",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                }).check();
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.SEND_SMS)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        updateLocation();
+                        Toast.makeText(MainActivity.this,"SMS linked",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(MainActivity.this,"You must accept",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                }).check();
+    }
+
+     @Override
+     public void onStart() {
+         super.onStart();
+         // Check if user is signed in (non-null) and update UI accordingly.
+         FirebaseUser currentUser = mAuth.getCurrentUser();
+         updateUI(currentUser);
+     }
+
+     private void updateUI(FirebaseUser user) {
+        if(user==null){
+            mAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Toast.makeText(MainActivity.this, "Logged in"+user.getUid(), Toast.LENGTH_LONG).show();
+                        updateUI(user);
+                    } else {
+                        updateUI(null);
+                    }
+                }
+            });
+        }
+        else{
+            //final String TAG = "login";
+            final String uid = user.getUid();
+            DatabaseReference newUser = FirebaseDatabase.getInstance()
+                    .getReference("users");
+            newUser.child(uid)
+                    .addListenerForSingleValueEvent(new ValueEventListener(){
+                @Override
+                public void onDataChange(DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        Log.i("fb", snapshot.getKey());
+                        MyLocationService.User_ID = snapshot.getKey();
+                        Intent intentTest = new Intent(MainActivity.this, MyLocationService.class);
+                        intentTest.setAction(MyLocationService.ACTION_AUTHOR_UPDATE);
+                        intentTest.putExtra("TEST_VAL",snapshot.getKey());
+                        sendBroadcast(intentTest);
+                    } else {
+                        Log.e("fb", "Not found: " + uid);
+                        snapshot.getRef().getParent().child(uid).child("LKL").setValue(true);
+                        snapshot.getRef().getParent().child(uid).child("Contacts").setValue(false);
+                    }
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Log.e("fb", databaseError.toString());
+                }
+            });
+        }
+     }
+
+     private void updateLocation() {
+         buildLocationRequest();
+
+         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+         if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+             return;
+         }
+         fusedLocationProviderClient.requestLocationUpdates(locationRequest, getPendingIntentLoc());
+     }
+     private PendingIntent getPendingIntentLoc(){
+         Intent intent = new Intent(this, MyLocationService.class);
+         intent.setAction(MyLocationService.ACTION_PROCESS_UPDATE);
+         return PendingIntent.getBroadcast(this,0,intent, PendingIntent.FLAG_UPDATE_CURRENT);
+     }
+
+     private void buildLocationRequest() {
+         locationRequest = new LocationRequest();
+         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+         locationRequest.setInterval(4000);
+         locationRequest.setFastestInterval(2000);
+         locationRequest.setSmallestDisplacement(1f);
+     }
+
+     public void updateTextView(final String value){
+         MainActivity.this.runOnUiThread(new Runnable() {
+             @Override
+             public void run() {
+                txt_location.setText(value);
+             }
+         });
+     }
+ }
---
+++ app/src/main/java/com/hovar/googlelocationbackground/MyLocationService.java
@@ -0,0 +1,78 @@
+package com.hovar.googlelocationbackground;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.location.Location;
+
+import android.util.Log;
+import android.widget.Toast;
+
+
+import com.google.android.gms.location.LocationResult;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class MyLocationService extends BroadcastReceiver {
+
+    public static final String ACTION_PROCESS_UPDATE="com.hovar.googlelocationbackground.UPDATE_LOCATION";
+    public static final String ACTION_AUTHOR_UPDATE="com.hovar.googlelocationbackground.UPDATE_AUTHOR";
+    SharedPreferences sharedpreferences;
+
+    public static boolean testt = false;
+    public static String User_ID="";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(intent!=null){
+            sharedpreferences = context.getSharedPreferences("customTestPref", Context.MODE_PRIVATE);
+            testt = sharedpreferences.getBoolean("testt",false);
+            User_ID = sharedpreferences.getString("User_ID","");
+            Log.i("TESTING","NON NULL intent");
+            Log.i("TESTING uid",User_ID);
+            final String action = intent.getAction();
+            Log.i("TESTING","finally should go in "+testt);
+            if(ACTION_PROCESS_UPDATE.equals(action) && testt){
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("locations").child(User_ID);
+                LocationResult result = LocationResult.extractResult(intent);
+                Log.i("results","RESULT INCOMING");
+                if(result!=null){
+                    Location location = result.getLastLocation();
+                    String location_string = new StringBuilder("Lat: "+location.getLatitude())
+                            .append(" , Long: ")
+                            .append(location.getLongitude())
+                            .append(" ,alt: ")
+                            .append(location.getAltitude())
+                            .toString();
+                    Log.i("LOC",location_string);
+                    myRef.child("Latitude").setValue(location.getLatitude());
+                    myRef.child("Longitude").setValue(location.getLongitude());
+                    myRef.child("Altitude").setValue(location.getAltitude());
+                    myRef.child("lastUpdatedAt").setValue(location.getTime());
+                    try{
+                        MainActivity.getInstance().updateTextView(location_string);
+                        //myRef.setValue(location_string);
+                        myRef.child("offline").setValue(false);
+                    }catch (Exception ex){
+                        Log.i("offline","working");
+                        Toast.makeText(context, location_string, Toast.LENGTH_SHORT).show();
+                        myRef.child("offline").setValue(true);
+                    }
+                }
+            }
+            if(ACTION_AUTHOR_UPDATE.equals(action)){
+                Log.i("TESTING",intent.getStringExtra("TEST_VAL"));
+                Log.i("TESTING","IT WORKS OH YEAH");
+                SharedPreferences.Editor edit = sharedpreferences.edit();
+                edit.putBoolean("testt",true);
+                edit.putString("User_ID",intent.getStringExtra("TEST_VAL"));
+                edit.apply();
+                //testt=true;
+            }
+        }
+    }
+}
